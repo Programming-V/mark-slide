2e0485f8538a319d86116f2eeca63b60f398a71a
José Luis Terán <joseluisteran2617@gmail.com>
2024-04-26 23:42:07 -0400

    feat: update main logic

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,20 @@
-module Main (main) where
+{-# LANGUAGE PackageImports #-}
 
-import Lib
+module Main 
+    ( main ) where
+
+import Interpreter (generateHTML, writeHTMLFile)
+import Scanner
+import Parser
+import UU.Parsing
+
 
 main :: IO ()
-main = someFunc
+main = do 
+          input <- readFile "slide.p5"
+          let tokens = scanTokens input
+          putStrLn (show tokens)
+          ast <- parseIO pSlides tokens
+          putStrLn (show ast)
+          let slides = generateHTML ast
+          writeHTMLFile "Presentation.html" slides
